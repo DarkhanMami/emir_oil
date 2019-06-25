0fe87857971416bb1abd26f9aa84b81cbacd91f0
DarkhanMami <dmami@nu.edu.kz>
2019-06-25 15:10:21 +0600

    run sh update

--- run.sh
+++ run.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-cd /webapps/nurotan/
+cd /webapps/emir_oil/
 pip3 install -r requirements.txt
 export DJANGO_SETTINGS_MODULE="system.settings"
 python3 manage.py collectstatic --noinput
 python3 manage.py migrate --noinput
-systemctl restart nurotan
+systemctl restart emir
 systemctl restart nginx
