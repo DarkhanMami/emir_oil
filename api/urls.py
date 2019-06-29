c9564fe77f22b5c5007928c15f12386edbdfb076
DarkhanMami <dmami@nu.edu.kz>
2019-06-29 14:12:34 +0600

    new fields

--- api/urls.py
+++ api/urls.py
@@ -8,8 +8,8 @@ app_name = 'api'
 
 router = DefaultRouter()
 router.register(r'well_matrix', views.WellMatrixViewSet, base_name='well_matrix')
-router.register(r'fields', views.FieldViewSetViewSet, base_name='fields')
-router.register(r'wells', views.WellViewSetViewSet, base_name='wells')
+router.register(r'fields', views.FieldViewSet, base_name='fields')
+router.register(r'wells', views.WellViewSet, base_name='wells')
 
 
 urlpatterns = [
